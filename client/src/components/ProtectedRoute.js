66a690f24139dc61e75ef8717d90e904b927482f
unknown <dethgrenade@gmail.com>
2020-05-22 08:31:35 -0700

    protected route created

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+    const token = localStorage.getItem('token')
+
+    return (
+        <Route
+            {...rest}
+            render={() => token ? <Component /> : <Redirect to='/login' />}
+        />
+    )
+}
+
+export default ProtectedRoute
